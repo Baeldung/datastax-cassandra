f378c322e9c74699fe14938df8080b7b0eea9632
Daniel Strmecki <daniel.strmecki@gmail.com>
2021-07-31 14:11:07 +0200

    BAEL-5017: use nested test classes

--- spring-cassandra/src/main/java/org/baeldung/springcassandra/model/Person.java
+++ spring-cassandra/src/main/java/org/baeldung/springcassandra/model/Person.java
@@ -1,7 +1,5 @@
 package org.baeldung.springcassandra.model;
 
-import lombok.Builder;
-import lombok.Data;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
